360641e56228ebb0adeb469047f72f5897471cd1
Lameck Irungu <mugolameck@gmail.com>
2025-11-18 08:25:00 +0300

    fix: replace permissions module with correct authentication module for session authentication settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.permissions.SessionAuthentication'
+        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
