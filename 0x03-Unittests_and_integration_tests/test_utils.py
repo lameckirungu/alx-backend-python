cfd267d74b738a5a928e36951e4d9edca6142cda
Lameck Irungu <mugolameck@gmail.com>
2025-11-26 11:57:34 +0300

    feat: Implement an exception test case using assert raises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,4 +17,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests access_nested_map returns correct value."""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Tests access_nested_map raises KeyError for invalid path with correct message.
+        
+        :param self: The test case instance.
+        :param nested_map: the nested map
+        :param path: the path
+        """
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
         
